333b54e6876120cf4cc3e175a485b3e3677565a3
jrbrayjr2006 <jrbrayjr2006@gmail.com>
2018-02-19 19:11:43 -0600

    Re-factored application for current Spring Boot 1.5.10 and added tests

--- src/main/java/com/jaydot2/server/controller/ContactUsController.java
+++ src/main/java/com/jaydot2/server/controller/ContactUsController.java
@@ -4,32 +4,41 @@ import com.jaydot2.server.dao.DAOMongoDBImpl;
 import com.jaydot2.server.model.ContactMessage;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created by jamesbray on 11/27/16.
+ * Created by jamesbray on 11/27/2016.
+ * Modified by jamesbray on 02/19/2018
  */
 @RestController
 @CrossOrigin
 @RequestMapping("/jaydot2/services")
-@EnableAutoConfiguration
 public class ContactUsController {
 
     private static final Logger LOG = LogManager.getLogger(ContactUsController.class);
 
+    @Autowired
     private DAOMongoDBImpl dao;
 
-    public static void main(String[] args) {
-        SpringApplication.run(ContactUsController.class, args);
+    /**
+     * Default service operation
+     * @return
+     */
+    @CrossOrigin
+    @RequestMapping( value = "/", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET )
+    public Map<String,String> home() {
+        Map<String,String> result = new HashMap<String, String>();
+        result.put("data","default service response");
+        return result;
     }
 
     @CrossOrigin
-    @RequestMapping(value="/insert/{email}/{message}")
+    @RequestMapping(value="/insert/{email}/{message}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String insertRecord(@PathVariable("email") String email, @PathVariable("message") String message) {
         LOG.debug("::ENTER:: ContactUsController.insertRecord(String,String)...");
         String result = null;
@@ -37,6 +46,21 @@ public class ContactUsController {
         dao = DAOMongoDBImpl.getInstance();
         dao.insertRecord(contactMessage);
         LOG.debug("::EXIT:: ContactUsController.insertRecord(String,String)...");
+        result = "success";
+        return result;
+    }
+
+    /**
+     * <p>
+     *     Operation used to add a new user record to the data repository
+     * </p>
+     * @return
+     */
+    @CrossOrigin
+    @RequestMapping( value = "/user", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE )
+    public boolean createUser() {
+        boolean result = false;
+
         return result;
     }
 }
---
+++ src/test/java/com/jaydot2/server/Jaydot2ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.jaydot2.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class Jaydot2ApplicationTest {
+
+    @Test
+    public void contextLoads() throws Exception {}
+}
